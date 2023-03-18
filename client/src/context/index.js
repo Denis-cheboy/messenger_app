8dc0047f8b462adda9cbc6ce824c14f45e3c5cf1
denisabonda <denocheboy@gmail.com>
2023-03-18 01:49:57 -0700

    second-ammending

--- client/src/context/index.js
+++ client/src/context/index.js
@@ -1,7 +1,7 @@
 import { createContext, useContext, useState } from "react";
 import io from "socket.io-client"
 
-const socket=io("http://localhost:3500")
+const socket=io("https://messenger-app-api.onrender.com")
 const AppContext=createContext()
 
 export const AppContextProvider=({children})=>{
